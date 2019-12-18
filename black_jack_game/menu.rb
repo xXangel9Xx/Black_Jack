66381ec067ca6129836c1a55f3788c32467adb68
xXangelXx <angeluchimag17@gmail.com>
2019-12-18 02:19:22 -0400

    arreglo ortografico en el menu

--- black_jack_game/menu.rb
+++ black_jack_game/menu.rb
@@ -4,7 +4,7 @@ puts "                                                                          
 puts "      ╔♫═╗╔╗ ♥                    ╔═════════════════════╗  __^__                 ٩(×̯×)۶               __^__      ".colorize(:white).on_black
 puts "      ╚╗╔╝║║♫═╦╦╦╔╗               ║   ¿Que desea hacer? ║ ( ___ )------------------------------------( ___ )     ".colorize(:white).on_black
 puts "      ╔╝╚╗♫╚╣║║║║╔╣               ║       1-Jugar       ║  | / |                                      | / |      ".colorize(:white).on_black
-puts "      ╚═♫╝╚═╩═╩♫╩═╝               ║ 2-Objetivo del juego║  | / |             Bien Venido              | / |      ".colorize(:white).on_black
+puts "      ╚═♫╝╚═╩═╩♫╩═╝               ║ 2-Objetivo del juego║  | / |             BienVenido               | / |      ".colorize(:white).on_black
 puts "  (¸¸.♥➷♥•*¨)¸.•´¸.•*¨) ¸.•*¨     ║  3-¿Como se juega?  ║  |___|                                      |___|      ".colorize(:white).on_black
 puts "  (¸.•´(¸. ¸.•´¸.•*¨) ¸.♥➷•*¨)    ║       4-Salir       ║ (_____)------------------------------------(_____)     ".colorize(:white).on_black
 puts "                                  ╚═════════════════════╝                                                        ".colorize(:white).on_black
